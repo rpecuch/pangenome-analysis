bf138a7bda3995c0b2363bcc702790fb17360d0b
userrp <109092365+rpecuch@users.noreply.github.com>
2023-02-01 10:30:08 -0500

    wrote fxn to direct user to page to download genomes

--- downloads.js
+++ downloads.js
@@ -2,8 +2,26 @@
 const speciesInput = document.querySelector('#speciesInput');
 const assemblyBtn = document.querySelector('#assemblyBtn');
 const orderbtn = document.querySelector('#orderBtn');
+const assemblyContainer = document.querySelector('#assemblyContainer');
+const orderContainer = document.querySelector('#orderContainer');
+const genomeHelp = document.querySelector('#genomeHelp');
+const orderHelp = document.querySelector('#orderHelp');
 
 // search fxns
-
+const findAssembly = (e) => {
+    e.preventDefault();
+    // retrieve genus and species from input box
+    const genus = speciesInput.value.split(' ')[0].toLowerCase();
+    const species = speciesInput.value.split(' ')[1].toLowerCase();
+    // create link
+    const linkEl = document.createElement('a');
+    linkEl.textContent = 'Download Assemblies';
+    linkEl.setAttribute('href', `https://www.ncbi.nlm.nih.gov/assembly/?term=${genus}+${species}`);
+    linkEl.setAttribute('target', '_blank');
+    assemblyContainer.append(linkEl);
+    // make user directions stand out
+    genomeHelp.classList.remove('text-muted');
+}
 
 // add event listeners to respective btns
+assemblyBtn.addEventListener('click', findAssembly);
