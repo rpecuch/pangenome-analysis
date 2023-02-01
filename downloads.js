a0651fb489bc484b05b498a56914d90a435a2a97
userrp <109092365+rpecuch@users.noreply.github.com>
2023-02-01 10:35:38 -0500

    wrote fxn to direct user to page to download original order file

--- downloads.js
+++ downloads.js
@@ -1,7 +1,7 @@
 // select needed elements and store them in variables
 const speciesInput = document.querySelector('#speciesInput');
 const assemblyBtn = document.querySelector('#assemblyBtn');
-const orderbtn = document.querySelector('#orderBtn');
+const orderBtn = document.querySelector('#orderBtn');
 const assemblyContainer = document.querySelector('#assemblyContainer');
 const orderContainer = document.querySelector('#orderContainer');
 const genomeHelp = document.querySelector('#genomeHelp');
@@ -23,5 +23,21 @@ const findAssembly = (e) => {
     genomeHelp.classList.remove('text-muted');
 }
 
+const findOrder = (e) => {
+    e.preventDefault();
+    // retrieve genus and species from input box
+    const genus = speciesInput.value.split(' ')[0].toLowerCase();
+    const species = speciesInput.value.split(' ')[1].toLowerCase();
+    // create link
+    const linkEl = document.createElement('a');
+    linkEl.textContent = 'Download Original Order';
+    linkEl.setAttribute('href', `https://www.ncbi.nlm.nih.gov/genome/browse#!/prokaryotes/${genus}%20${species}`);
+    linkEl.setAttribute('target', '_blank');
+    orderContainer.append(linkEl);
+    // make user directions stand out
+    orderHelp.classList.remove('text-muted');
+}
+
 // add event listeners to respective btns
 assemblyBtn.addEventListener('click', findAssembly);
+orderBtn.addEventListener('click', findOrder);
