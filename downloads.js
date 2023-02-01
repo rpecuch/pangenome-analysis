fdd1dfff651df9061f4cf4a4824df229e36b3d9e
userrp <109092365+rpecuch@users.noreply.github.com>
2023-01-31 21:01:05 -0500

    set up basic structure of js file to perform search fxns

---
+++ downloads.js
@@ -0,0 +1,9 @@
+// select needed elements and store them in variables
+const speciesInput = document.querySelector('#speciesInput');
+const assemblyBtn = document.querySelector('#assemblyBtn');
+const orderbtn = document.querySelector('#orderBtn');
+
+// search fxns
+
+
+// add event listeners to respective btns
