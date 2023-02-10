00f61a0e05aa3eefea222e1b8a30cd4e67b1a595
userrp <109092365+rpecuch@users.noreply.github.com>
2023-02-09 20:17:08 -0500

    created one single validation fxn to use with both search fxns

--- downloads.js
+++ downloads.js
@@ -8,12 +8,8 @@ const orderLink = document.querySelector('#orderLink');
 const genomeHelp = document.querySelector('#genomeHelp');
 const orderHelp = document.querySelector('#orderHelp');
 
-// next steps
-    // new search
-
-// search fxns
-const findAssembly = (e) => {
-    e.preventDefault();
+// search validation fxn
+const checkSearch = () => {
     // prevent empty search
     if (!speciesInput.value) {
         helperText.textContent = 'Search cannot be empty!';
@@ -25,28 +21,42 @@ const findAssembly = (e) => {
         return;
     }
     helperText.textContent = '';
-    // retrieve genus and species from input box
-    const genus = speciesInput.value.split(' ')[0].toLowerCase();
-    const species = speciesInput.value.split(' ')[1].toLowerCase();
-    // create link
-    assemblyLink.textContent = 'Download Assemblies';
-    assemblyLink.setAttribute('href', `https://www.ncbi.nlm.nih.gov/assembly/?term=${genus}+${species}`);
-    // make user directions stand out
-    genomeHelp.classList.remove('text-muted');
-    orderHelp.classList.add('text-muted');
+    return speciesInput;
+}
+
+// search fxns
+const findAssembly = (e) => {
+    e.preventDefault();
+    // validation
+    checkSearch();
+    if (checkSearch() !== undefined) {
+        // retrieve genus and species from input box
+        const genus = speciesInput.value.split(' ')[0].toLowerCase();
+        const species = speciesInput.value.split(' ')[1].toLowerCase();
+        // create link
+        assemblyLink.textContent = 'Download Assemblies';
+        assemblyLink.setAttribute('href', `https://www.ncbi.nlm.nih.gov/assembly/?term=${genus}+${species}`);
+        // make user directions stand out
+        genomeHelp.classList.remove('text-muted');
+        orderHelp.classList.add('text-muted');
+    }
 }
 
 const findOrder = (e) => {
     e.preventDefault();
-    // retrieve genus and species from input box
-    const genus = speciesInput.value.split(' ')[0].toLowerCase();
-    const species = speciesInput.value.split(' ')[1].toLowerCase();
-    // create link
-    orderLink.textContent = 'Download Original Order';
-    orderLink.setAttribute('href', `https://www.ncbi.nlm.nih.gov/genome/browse#!/prokaryotes/${genus}%20${species}`);
-    // make user directions stand out
-    orderHelp.classList.remove('text-muted');
-    genomeHelp.classList.add('text-muted');
+    // validation
+    checkSearch();
+    if (checkSearch() !== undefined) {
+        // retrieve genus and species from input box
+        const genus = speciesInput.value.split(' ')[0].toLowerCase();
+        const species = speciesInput.value.split(' ')[1].toLowerCase();
+        // create link
+        orderLink.textContent = 'Download Original Order';
+        orderLink.setAttribute('href', `https://www.ncbi.nlm.nih.gov/genome/browse#!/prokaryotes/${genus}%20${species}`);
+        // make user directions stand out
+        orderHelp.classList.remove('text-muted');
+        genomeHelp.classList.add('text-muted');
+    }
 }
 
 // add event listeners to respective btns
