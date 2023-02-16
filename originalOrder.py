de5918d3311ecb8003fce9bf6d59e131ff2369d3
userrp <109092365+rpecuch@users.noreply.github.com>
2023-02-15 20:18:57 -0500

    added user input for genus and species

--- originalOrder.py
+++ originalOrder.py
@@ -1,5 +1,10 @@
-# will need some type of user input because the path to downloads folder will be different on everyone's computer
+# will need some type of user input because the path to downloads folder will be different on everyone's computer, or can just adjust the script for each user at the very beginning and then it will always run 
 import os
 import shutil
-os.rename('/Users/ritapecuch/Downloads/prokaryotes.csv', '/Users/ritapecuch/Downloads/Species_name.csv')
+# eventually can change this so at beginning of pipeline we take in genus and species and then use for all scripts
+organism = input('Enter your organism in the following format: Genus species --> ')
+genus = organism.split()[0].capitalize()
+species = organism.split()[1]
+# todo: eventually add validation
+# os.rename('/Users/ritapecuch/Downloads/prokaryotes.csv', '/Users/ritapecuch/Downloads/Species_name.csv')
 # shutil.move("path/to/current/file.foo", "path/to/new/destination/for/file.foo")
