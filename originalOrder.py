76269435192a8ed9371bee048cafabc4a58a284c
userrp <109092365+rpecuch@users.noreply.github.com>
2023-02-15 20:08:22 -0500

    created first python script and added command to rename original order file

---
+++ originalOrder.py
@@ -0,0 +1,5 @@
+# will need some type of user input because the path to downloads folder will be different on everyone's computer
+import os
+import shutil
+os.rename('/Users/ritapecuch/Downloads/prokaryotes.csv', '/Users/ritapecuch/Downloads/Species_name.csv')
+# shutil.move("path/to/current/file.foo", "path/to/new/destination/for/file.foo")
