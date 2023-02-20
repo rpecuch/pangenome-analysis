670158184caab943e1ec1443d0bc1ff2597391da
userrp <109092365+rpecuch@users.noreply.github.com>
2023-02-20 08:35:27 -0500

    created variable to store path to a user's downloads folder

--- originalOrder.py
+++ originalOrder.py
@@ -1,10 +1,12 @@
 # will need some type of user input because the path to downloads folder will be different on everyone's computer, or can just adjust the script for each user at the very beginning and then it will always run 
 import os
 import shutil
+# this will need to be adjusted for each user
+downloadsPath = '/Users/ritapecuch/Downloads'
 # eventually can change this so at beginning of pipeline we take in genus and species and then use for all scripts
 organism = input('Enter your organism in the following format: Genus species --> ')
 genus = organism.split()[0].capitalize()
 species = organism.split()[1]
 # todo: eventually add validation
-# os.rename('/Users/ritapecuch/Downloads/prokaryotes.csv', '/Users/ritapecuch/Downloads/Species_name.csv')
-# shutil.move("path/to/current/file.foo", "path/to/new/destination/for/file.foo")
+os.rename(f'{downloadsPath}/prokaryotes.csv', f'{downloadsPath}/Species_name.csv')
+# shutil.move("/Users/ritapecuch/Downloads/Species_name.csv", "/Volumes/Pangenome/FirstLast/Species_genus/OriginalOrder")
