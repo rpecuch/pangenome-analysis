f4977a0786829b278cf12a7fbf416a0f0f54bb81
userrp <109092365+rpecuch@users.noreply.github.com>
2023-02-20 12:44:41 -0500

    added validation for when user enters genus and species

--- originalOrder.py
+++ originalOrder.py
@@ -1,4 +1,3 @@
-# will need some type of user input because the path to downloads folder will be different on everyone's computer, or can just adjust the script for each user at the very beginning and then it will always run 
 import os
 import shutil
 # this will need to be adjusted for each user
@@ -8,9 +7,16 @@ fName = 'Rita'
 lName = 'Pecuch'
 # eventually can change this so at beginning of pipeline we take in genus and species and then use for all scripts
 organism = input('Enter your organism in the following format: Genus species --> ')
-genus = organism.split()[0].capitalize()
-species = organism.split()[1]
-# todo: eventually add validation
-# todo: connect to network drive to test this
-os.rename(f'{downloadsPath}/prokaryotes.csv', f'{downloadsPath}/{genus}_{species}.csv')
-shutil.move(f'{downloadsPath}/{genus}_{species}.csv', f'{userDrivePath}/{fName}{lName}/{genus}_{species}/OriginalOrder')
+# prevent empty search and check format
+if not organism:
+    print('Error: organism cannot be blank.')
+elif len(organism.split(' ')) != 2:
+    print('Error: enter the genus and species, separated by a space.')
+else:
+    genus = organism.split()[0].capitalize()
+    species = organism.split()[1]
+    print(f'{genus} {species}')
+    # rename file
+    os.rename(f'{downloadsPath}/prokaryotes.csv', f'{downloadsPath}/{genus}_{species}.csv')
+    # move to network drive
+    shutil.move(f'{downloadsPath}/{genus}_{species}.csv', f'{userDrivePath}/{fName}{lName}/{genus}_{species}/OriginalOrder')
